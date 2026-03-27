9b5b07fc6e19ab75b35292a8a7715b9ba6343a71
Anthony Nelson <anthonyjnelson@hotmail.com>
2026-03-27 16:20:56 +0000

    Registration API created

--- backend-drf/stock_prediction_main/settings.py
+++ backend-drf/stock_prediction_main/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # 3rd Party Apps
+    "rest_framework",
+    "accounts",
+    "api",
 ]
 
 MIDDLEWARE = [
