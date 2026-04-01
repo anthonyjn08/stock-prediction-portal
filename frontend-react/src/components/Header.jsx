c3e42676b61061d3ab6909708b70aa4ab17d2020
Anthony Nelson <anthonyjnelson@hotmail.com>
2026-04-01 16:59:04 +0100

    Add registration page

--- frontend-react/src/components/Header.jsx
+++ frontend-react/src/components/Header.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import Button from "./Button";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <>
       <nav className="navbar container py-3 align-items-start">
-        <a className="navbar-brand text-light">Stock Prediction Portal</a>
+        <Link className="navbar-brand text-light" to="/">
+          Stock Prediction Portal
+        </Link>
 
         <div>
-          <Button text="login" class="btn-outline-info" />
+          <Button text="login" class="btn-outline-info" url="/login" />
           &nbsp;
-          <Button text="register" class="btn-info" />
+          <Button text="register" class="btn-info" url="/register" />
         </div>
       </nav>
     </>
